673deaafe595ab05dd76e3340f4d425974991ceb
absolutelycold_Windows <36677040@qq.com>
2018-04-29 00:04:20 +0800

    The structure of project

---
+++ config.py
@@ -0,0 +1,16 @@
+import os
+
+# This is a secrete key for session
+SECRETE_KEY = os.urandom(24)
+# Debug mode
+DEBUG = True
+# The setting for database
+DIALECT = 'mysql'
+DRIVER = 'pymysql'
+USERNAME = 'root'
+PASSWORD = '123'
+ADDRESS = '127.0.0.1'
+PORT = '3306'
+DATABASE = 'flask_study'
+SQLALCHEMY_DATABASE_URI = '{}+{}://{}:{}@{}:{}/{}'.format(DIALECT, DRIVER, USERNAME, PASSWORD, ADDRESS, PORT, DATABASE)
+
