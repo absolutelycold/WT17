b850b8ff5a3076ad087ad5b2ff426bc1206c3893
absolutelycold_Windows <36677040@qq.com>
2018-04-29 11:36:25 +0800

    Complete register

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import config
+from exts import db
+from models import Users
 
 app = Flask(__name__)
 app.config.from_object(config)
+db.init_app(app)
 
 
 @app.route('/')
@@ -15,9 +18,38 @@ def login():
     return render_template('login.html')
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('register.html')
+    if request.method == 'GET':
+        return render_template('register.html')
+    else:
+        name = request.form.get('name').strip()
+        account = request.form.get('account').strip()
+        password = request.form.get('password').strip()
+        repass = request.form.get('repassword').strip()
+
+        # 检查重名
+        user1 = Users.query.filter(Users.name == name).first()
+        if user1:
+            status = 1
+            return render_template('register.html', status=status)
+        else:
+            user2 = Users.query.filter(Users.account == account).first()
+            if user2:
+                status = 2
+                return render_template('register.html', status=status)
+            elif password != repass:
+                status = 3
+                return render_template('register.html', status=status)
+            elif (password == '') or (repass == '') or (name == '') or (account == ''):
+                status = 4
+                return render_template('register.html', status=status)
+            else:
+                newUser = Users(name=name, account=account, password=password)
+                db.session.add(newUser)
+                db.session.commit()
+                status = 5
+                return render_template('register.html', status=status)
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -1,1 +1,9 @@
 from exts import db
+
+
+class Users(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(50), nullable=False)
+    account = db.Column(db.String(20), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
